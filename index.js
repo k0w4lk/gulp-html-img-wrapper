864b4e608d18657c7c295d3d2da0b95d5aacc77f
k0w4lk <evgeniyreut3@gmail.com>
2022-06-25 16:58:36 +0300

    add responsive images + fixes

--- index.js
+++ index.js
@@ -27,11 +27,16 @@ const gulpHtmlImgWrapper = function (userParams) {
         ...userParams,
       };
 
-      const EXCLUDE_ATTR = 'ghiw-exclude';
-
+      const EXCLUDE_ATTR_REGEX = /<img[^>]*(ghiw-exclude)[^>]*>/i;
       const EXTENSION_REGEX = /(?<=src=[\'\"][\w\W]*)\.[\w]+(?=[\'\"])/i;
       const PICTURE_CLASS_REGEX =
-        /<img[^>]*(pictureClass=([\"\']\S+[\"\']))[^>]*>/i;
+        /<img[^>]*(ghiw-picture-class=([\"\'].+[\"\']))[^>]*>/i;
+      const RESPONSIVE_FILENAMES_REGEX =
+        /<img[^>]*(ghiw-responsive-filenames=[\"\'](.+)[\"\'])[^>]*>/i;
+      const RESPONSIVE_MEDIA_REGEX =
+        /<img[^>]*(ghiw-responsive-media=[\"\'](.+)[\"\'])[^>]*>/i;
+      const RESPONSIVE_EXTENSIONS_REGEX =
+        /<img[^>]*(ghiw-responsive-extensions=[\"\'](.+)[\"\'])[^>]*>/i;
       const IMG_SRC_REGEX = /<img[^>]*src=[\"\'](\S+)[\"\'][^>]*>/i;
       const IMG_REGEX = /<img[^>]*src=[\"|']([^\"\s]+)[\"|'][^>]*>/gi;
       const PICTURE_REGEX = /<\s*picture[^>]*>([\w\W]*?)<\s*\/\s*picture\s*>/gi;
@@ -64,12 +69,26 @@ const gulpHtmlImgWrapper = function (userParams) {
         newHtml = noImagesHtml;
 
         const newImages = images.map((image) => {
-          if (image.includes(EXCLUDE_ATTR)) {
-            if (PICTURE_CLASS_REGEX.test(image)) {
-              const picClassAttr = image.match(PICTURE_CLASS_REGEX)[1];
-              image = image.replace(picClassAttr, '');
-            }
-            return image.replace(EXCLUDE_ATTR, '');
+          const ghiwExcludeAttr = image.match(EXCLUDE_ATTR_REGEX);
+          const ghiwPictureClass = image.match(PICTURE_CLASS_REGEX);
+          const ghiwResponsiveFilenames = image.match(
+            RESPONSIVE_FILENAMES_REGEX
+          );
+          const ghiwResponsiveExtensions = image.match(
+            RESPONSIVE_EXTENSIONS_REGEX
+          );
+          const ghiwResponsiveMedias = image.match(RESPONSIVE_MEDIA_REGEX);
+
+          const removeCustomAttributes = (image) =>
+            image
+              .replace(ghiwExcludeAttr?.[1], '')
+              .replace(ghiwPictureClass?.[1], '')
+              .replace(ghiwResponsiveFilenames?.[1], '')
+              .replace(ghiwResponsiveExtensions?.[1], '')
+              .replace(ghiwResponsiveMedias?.[1], '');
+
+          if (ghiwExcludeAttr) {
+            return removeCustomAttributes(image);
           }
 
           if (EXTENSION_REGEX.test(image)) {
@@ -77,31 +96,64 @@ const gulpHtmlImgWrapper = function (userParams) {
             const srcValueWithoutExt = image
               .match(IMG_SRC_REGEX)[1]
               .replace(imageExt, '');
-            let pictureClass;
 
             if (!params.extensions.includes(imageExt)) {
-              return image;
+              return removeCustomAttributes(image);
             }
 
-            if (PICTURE_CLASS_REGEX.test(image)) {
-              pictureClass = image.match(PICTURE_CLASS_REGEX)[2];
-              image = image.replace(`pictureClass=${pictureClass}`, '');
+            if (ghiwPictureClass) {
+              image = image.replace(ghiwPictureClass[1], '');
             }
 
-            const newTag =
-              '<picture' +
-              `${pictureClass ? ' class=' + pictureClass : ''}` +
-              '>' +
-              '<source srcset="' +
-              srcValueWithoutExt +
-              '.webp" type="image/webp">' +
-              image +
-              '</picture>';
+            if (
+              RESPONSIVE_MEDIA_REGEX.test(image) &&
+              RESPONSIVE_FILENAMES_REGEX.test(image) &&
+              RESPONSIVE_EXTENSIONS_REGEX.test(image)
+            ) {
+              let sources = [];
+              const extensions = image
+                .match(RESPONSIVE_EXTENSIONS_REGEX)[2]
+                .split(';')
+                .map((ext) => ext.trim());
+
+              const medias = image
+                .match(RESPONSIVE_MEDIA_REGEX)[2]
+                .split(';')
+                .map((media) => media.trim());
+
+              const filenames = image
+                .match(RESPONSIVE_FILENAMES_REGEX)[2]
+                .split(';')
+                .map((filename) => filename.trim());
+
+              extensions.forEach((ext) => {
+                medias.forEach((media, index) => {
+                  sources.push(
+                    `<source srcset="${filenames[index]}.${ext}" media="(${media})" type="image/${ext}" />`
+                  );
+                });
+              });
+
+              image = removeCustomAttributes(image);
+
+              const newTag = `<picture${
+                ghiwPictureClass ? ' class=' + ghiwPictureClass[2] : ''
+              }>${sources.join('')}${image}</picture>`;
+
+              wrappedImagesCount++;
+
+              return newTag;
+            }
+
+            const newTag = `<picture${
+              ghiwPictureClass ? ' class=' + ghiwPictureClass[2] : ''
+            }><source srcset="${srcValueWithoutExt}.webp" type="image/webp">${image}</picture>`;
 
             wrappedImagesCount++;
 
             return newTag;
           }
+
           return image;
         });
 
@@ -137,7 +189,10 @@ const gulpHtmlImgWrapper = function (userParams) {
       if (wrappedImagesCount && params.logger) {
         const logMessage =
           wrappedImagesCount === 1 ? 'image was' : 'images were';
-        log(`${pluginName}:`, `${wrappedImagesCount} ${logMessage} wrapped`);
+        log(
+          `${pluginName}:`,
+          `${wrappedImagesCount} ${logMessage} wrapped in ${file.relative}`
+        );
       }
     } catch (err) {
       this.emit('error', new PluginError(pluginName, err));
