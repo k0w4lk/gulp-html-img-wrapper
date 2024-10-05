7b27f70ade9a8d87635a2d3c312d824abe4d1fb4
kowalk <evgeniyreut3@gmail.com>
2024-10-05 13:56:31 +0300

    refactoring

--- index.ts
+++ index.ts
@@ -10,6 +10,14 @@ interface UserParams {
 
 const pluginName = "gulp-html-img-wrapper";
 
+const attrs = {
+  exclude: "ghiw-exclude",
+  pictureClass: "ghiw-picture-class",
+  responsiveFilenames: "ghiw-responsive-filenames",
+  responsiveMedia: "ghiw-responsive-media",
+  responsiveExtensions: "ghiw-responsive-extensions",
+};
+
 export const gulpHtmlImgWrapper = function (userParams: UserParams = {}) {
   return through2.obj(function (file, enc, cb) {
     if (file.isNull()) {
@@ -47,64 +55,62 @@ export const gulpHtmlImgWrapper = function (userParams: UserParams = {}) {
       const removeCustomAttributes = (
         image: HTMLImageElement
       ): HTMLImageElement => {
-        image.removeAttribute("ghiw-exclude");
-        image.removeAttribute("ghiw-picture-class");
-        image.removeAttribute("ghiw-responsive-filenames");
-        image.removeAttribute("ghiw-responsive-media");
-        image.removeAttribute("ghiw-responsive-extensions");
+        image.removeAttribute(attrs.exclude);
+        image.removeAttribute(attrs.pictureClass);
+        image.removeAttribute(attrs.responsiveFilenames);
+        image.removeAttribute(attrs.responsiveMedia);
+        image.removeAttribute(attrs.responsiveExtensions);
 
         return image;
       };
 
-      let used = 0;
-
       images.forEach((image) => {
-        if (image.parentNode?.nodeName?.toLowerCase() === "picture") return;
-
-        if (image.hasAttribute("ghiw-exclude")) {
-          removeCustomAttributes(image);
-
-          return;
-        }
-
         const imageExt = image.src.match(imageExtRegExp)?.[1];
 
-        if (!imageExt || !params.extensions?.includes(imageExt)) {
+        const imageAlreadyInPicture =
+          image.parentNode?.nodeName?.toLowerCase() === "picture";
+        const imageExcluded = image.hasAttribute(attrs.exclude);
+        const imageHasUndeclaredExt = !params.extensions?.includes(
+          imageExt || ""
+        );
+
+        if (
+          imageAlreadyInPicture ||
+          imageExcluded ||
+          !imageExt ||
+          imageHasUndeclaredExt
+        ) {
           removeCustomAttributes(image);
 
           return;
         }
-
-        used++;
 
         const picture = document.createElement("picture");
         const imagePathWithoutExt = image.src.replace(`.${imageExt}`, "");
 
-        if (image.hasAttribute("ghiw-picture-class")) {
+        if (image.hasAttribute(attrs.pictureClass)) {
           picture.classList.add(
-            ...image.getAttribute("ghiw-picture-class")!.split(" ")
+            ...image.getAttribute(attrs.pictureClass)!.split(" ")
           );
-
-          image.removeAttribute("ghiw-picture-class");
         }
 
         if (
-          image.hasAttribute("ghiw-responsive-media") &&
-          image.hasAttribute("ghiw-responsive-filenames") &&
-          image.hasAttribute("ghiw-responsive-extensions")
+          image.hasAttribute(attrs.responsiveMedia) &&
+          image.hasAttribute(attrs.responsiveFilenames) &&
+          image.hasAttribute(attrs.responsiveExtensions)
         ) {
           const extensions = image
-            .getAttribute("ghiw-responsive-extensions")!
+            .getAttribute(attrs.responsiveExtensions)!
             .split(";")
             .map((ext) => ext.trim());
 
           const medias = image
-            .getAttribute("ghiw-responsive-media")!
+            .getAttribute(attrs.responsiveMedia)!
             .split(";")
             .map((ext) => ext.trim());
 
           const filenames = image
-            .getAttribute("ghiw-responsive-filenames")!
+            .getAttribute(attrs.responsiveFilenames)!
             .split(";")
             .map((ext) => ext.trim());
 
@@ -128,13 +134,12 @@ export const gulpHtmlImgWrapper = function (userParams: UserParams = {}) {
           picture.append(source);
         }
 
-        image = removeCustomAttributes(image);
+        removeCustomAttributes(image);
 
-        const clone = image.cloneNode();
-
-        picture.append(clone);
+        picture.append(image.cloneNode());
 
         image.replaceWith(picture);
+
         wrappedImagesCount++;
       });
 
